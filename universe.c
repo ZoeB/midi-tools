fc73295d64f9816315f9a65a95ee6824c5f19b6f
Zoë Blade <zoe@bytenoise.co.uk>
2020-12-19 15:04:32 +0000

    Make explicit, comment as I go

--- universe.c
+++ universe.c
@@ -5,6 +5,11 @@
 /* See midi.pdf page 131, "Conventions" */
 
 uint32_t readVariableLengthQuantity(FILE *inputFilePointer) {
+
+	/*
+	 * Read a 1 to 4 byte number
+	 */
+
 	uint32_t quantity = 0;
 	uint8_t  byte = 0;
 
