aacc6ef7ea2077739ed49dfea8080a306fb0bfb2
Zoë Blade <zoe@bytenoise.co.uk>
2020-12-21 14:30:40 +0000

    Split out interpreting MIDI events too

--- events.c
+++ events.c
@@ -22,11 +22,6 @@ void readEvent(FILE *inputFilePointer, uint32_t *position, uint8_t *status) {
 	uint8_t  dataBytesRead = 0;
 	uint32_t bytesToSkip = 0; /* For System Exclusive Messages */
 
-	char noteLetter[12] = "CCDDEFFGGAAB";
-	char noteIntonation[12] = "-#-#--#-#-#-";
-	int octave;
-	int pitch;
-
 	byte = getc(inputFilePointer);
 	(*position)++;
 
@@ -174,26 +169,5 @@ void readEvent(FILE *inputFilePointer, uint32_t *position, uint8_t *status) {
 		dataBytesRead++;
 	}
 
-	switch (statusNibbles[0]) {
-	case 0x08:
-	case 0x09:
-
-		if (statusNibbles[0] == 0x08) {
-			printf("Note-Off, ");
-		} else { /* statusNibbles[0] == 0x09 */
-			printf("Note-On, ");
-		}
-
-		octave = (dataBytes[0] / 12) - 1;
-		pitch = dataBytes[0] % 12;
-
-		printf("channel %Xh, %c%c%i, velocity %02Xh\n", statusNibbles[1], noteLetter[pitch], noteIntonation[pitch], octave, dataBytes[1]);
-		break;
-
-	case 0x0B:
-		printf("Control Change, channel %Xh, controller %02Xh, value %02Xh\n", statusNibbles[1], dataBytes[0], dataBytes[1]); /* See midi.pdf page 102, "Table III: Controller Numbers" */
-		break;
-	default:
-		printf("status %02Xh, data %02Xh %02Xh\n", *status, dataBytes[0], dataBytes[1]);
-	}
+	interpretMIDIEvent(status, statusNibbles, dataBytes); /* Only interpret, as we've already read the bytes */
 }
--- midi-ev.c
+++ midi-ev.c
@@ -2,3 +2,32 @@
  * MIDI event-level functions
  */
 
+void interpretMIDIEvent(uint8_t *status, uint8_t *statusNibbles, uint8_t *dataBytes) {
+	char noteLetter[12] = "CCDDEFFGGAAB";
+	char noteIntonation[12] = "-#-#--#-#-#-";
+	int octave;
+	int pitch;
+
+	switch (statusNibbles[0]) {
+	case 0x08:
+	case 0x09:
+
+		if (statusNibbles[0] == 0x08) {
+			printf("Note-Off, ");
+		} else { /* statusNibbles[0] == 0x09 */
+			printf("Note-On, ");
+		}
+
+		octave = (dataBytes[0] / 12) - 1;
+		pitch = dataBytes[0] % 12;
+
+		printf("channel %Xh, %c%c%i, velocity %02Xh\n", statusNibbles[1], noteLetter[pitch], noteIntonation[pitch], octave, dataBytes[1]);
+		break;
+
+	case 0x0B:
+		printf("Control Change, channel %Xh, controller %02Xh, value %02Xh\n", statusNibbles[1], dataBytes[0], dataBytes[1]); /* See midi.pdf page 102, "Table III: Controller Numbers" */
+		break;
+	default:
+		printf("status %02Xh, data %02Xh %02Xh\n", *status, dataBytes[0], dataBytes[1]);
+	}
+}
